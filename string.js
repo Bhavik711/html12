226f465d98dc4b21ade63b31923681b61cf0479b
Bhavik711 <bhavikgosavi711@gmail.com>
2025-01-20 10:56:27 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,6 @@
+var str="vasantdada patil college"
+console.log(str)
+console.log(str.length);
+console.log(str.charAt(1));
+console.log(str.startsWith("vasantdada"))
+console.log(str.endsWith("college"))
